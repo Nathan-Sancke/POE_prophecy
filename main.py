553621cdb059b4ae2eaf8a1a866fede3f56b8f95
Nathan-Sancke <73235503+Nathan-Sancke@users.noreply.github.com>
2021-12-13 22:42:07 +0100

    run the code when ctr+alt+f is pressed

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import cv2  # An image proccessing library
 import pytesseract  # an image to text library
 import pyautogui  # library to take screenshot
+import keyboard
 
 # Configure the module
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 path = r'C:\Users\natha\PycharmProjects\POE_prophecy\screenshot.PNG'
-img = pyautogui.screenshot(region=(250, 320, 170, 34))
-img.save(path)
-img = cv2.imread(path)
-# Resize the image
-new_dim = (int(img.shape[1] * 10), int(img.shape[0] * 10))
-img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
-# Change contrast (alpha) and brightness (beta)
-img = cv2.convertScaleAbs(img, alpha=1.4, beta=80)
-cv2.imshow('img traiter1', img)
-# Use OCR to read the text from the image
-out_below = pytesseract.image_to_string(img)
-# Print the text
-print(out_below)
-cv2.imshow('img traiter2', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+while True:
+    if keyboard.is_pressed('ctrl+alt+f'):  # activates the script when you do ctrl+alt+d
+        img = pyautogui.screenshot(region=(250, 320, 170, 34))  # take a screenshot
+        img.save(path)
+        img = cv2.imread(path)
+        # Resize the image
+        new_dim = (int(img.shape[1] * 10), int(img.shape[0] * 10))
+        img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
+        # Change contrast (alpha) and brightness (beta)
+        img = cv2.convertScaleAbs(img, alpha=1.4, beta=80)
+        cv2.imshow('img traiter1', img)
+        # Use OCR to read the text from the image
+        out_below = pytesseract.image_to_string(img)
+        # Print the text
+        print(out_below)
