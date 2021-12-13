ee33611c6401338007a191e09a11641b182e23a6
Nathan-Sancke <73235503+Nathan-Sancke@users.noreply.github.com>
2021-12-13 22:13:30 +0100

    add of the screenshot and adjusting of the contrast

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import cv2  # An image proccessing library
 import pytesseract  # an image to text library
+import pyautogui  # library to take screenshot
 
 # Configure the module
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-path = r'C:\Users\natha\PycharmProjects\POE_prophecy\Capture3.PNG'
+path = r'C:\Users\natha\PycharmProjects\POE_prophecy\screenshot.PNG'
+img = pyautogui.screenshot(region=(250, 320, 170, 34))
+img.save(path)
 img = cv2.imread(path)
 # Resize the image
 new_dim = (int(img.shape[1] * 10), int(img.shape[0] * 10))
 img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
 # Change contrast (alpha) and brightness (beta)
-img = cv2.convertScaleAbs(img, alpha=2, beta=80)
+img = cv2.convertScaleAbs(img, alpha=1.4, beta=80)
 cv2.imshow('img traiter1', img)
 # Use OCR to read the text from the image
 out_below = pytesseract.image_to_string(img)
