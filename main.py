ff184a5226b3680c27be05a23968a7772f48da41
Nathan-Sancke <73235503+Nathan-Sancke@users.noreply.github.com>
2021-12-14 00:06:57 +0100

    detect if the prophecy is in the prophecy_overview list

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import cv2  # An image proccessing library
-import pytesseract  # an image to text library
-import pyautogui  # library to take screenshot
 import keyboard
+import pyautogui  # library to take screenshot
+import pytesseract  # an image to text library
+from prophecy_overview import prophecy_overview
 
-# Configure the module
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-path = r'C:\Users\natha\PycharmProjects\POE_prophecy\screenshot.PNG'
-while True:
-    if keyboard.is_pressed('ctrl+alt+f'):  # activates the script when you do ctrl+alt+d
-        img = pyautogui.screenshot(region=(250, 320, 170, 34))  # take a screenshot
-        img.save(path)
-        img = cv2.imread(path)
-        # Resize the image
-        new_dim = (int(img.shape[1] * 10), int(img.shape[0] * 10))
-        img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
-        # Change contrast (alpha) and brightness (beta)
-        img = cv2.convertScaleAbs(img, alpha=1.4, beta=80)
-        cv2.imshow('img traiter1', img)
-        # Use OCR to read the text from the image
-        out_below = pytesseract.image_to_string(img)
-        # Print the text
-        print(out_below)
+if __name__ == '__main__':
+    # Configure the module
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    path = r'C:\Users\natha\PycharmProjects\POE_prophecy\screenshot.PNG'
+    while True:
+        if keyboard.is_pressed('ctrl+alt+f'):  # activates the script when you do ctrl+alt+d
+            img = pyautogui.screenshot(region=(250, 320, 170, 34))  # take a screenshot
+            img.save(path)
+            img = cv2.imread(path)
+            # Resize the image
+            new_dim = (int(img.shape[1] * 10), int(img.shape[0] * 10))
+            img = cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
+            # Change contrast (alpha) and brightness (beta)
+            img = cv2.convertScaleAbs(img, alpha=1.4, beta=80)
+            # Use OCR to read the text from the image
+            out_below = pytesseract.image_to_string(img)
+            x = out_below.split()
+            final = ""
+            for i in x:
+                final += i + " "
+            final = final[:-1]
+            # Print the text
+            if final in prophecy_overview:
+                print('money')
---
+++ prophecy_overview.py
@@ -0,0 +1,14 @@
+prophecy_overview = ["THE QUEEN'S SACRIFICE",
+                     "TRASH TO TREASURE",
+                     "FATED CONNECTIONS",
+                     "SONG OF THE SEKHEMA",
+                     "A DISHOUNORABLE DEATH",
+                     "BATTLE HARDENED",
+                     "DARKTONGUE'S SHRIEK",
+                     "THE TWINS",
+                     "WIND AND THUNDER",
+                     "MONSTROUS TREASURE",
+                     "THE MENTOR",
+                     "VALL WINDS",
+                     "A VALUABLE COMBINATION"
+                     ]
